940903e361e7d7a1c8d601955418f290c4719489
pyoun <59079426+developyoun@users.noreply.github.com>
2021-05-23 23:37:20 +0900

    :sparkles: apply debounce

--- src/component/header/SearchButton.js
+++ src/component/header/SearchButton.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import useDebounce from "component/common/useDebounce";
 
-import { TextField, InputAdornment, Grid } from "@material-ui/core";
+import { TextField, Grid } from "@material-ui/core";
 import { Search } from "@material-ui/icons";
 import styled from "styled-components";
 
@@ -13,13 +14,10 @@ const ButtonContainer = styled.div`
 const SearchButtonIcon = styled(Search)`
   color: white;
   font-size: 3.5rem;
-
   &:hover{
     cursor: pointer;
   }
 `;
-
-
 const InputBar = styled(TextField)`
   Input{
     font-size: 1.6rem;
@@ -27,53 +25,37 @@ const InputBar = styled(TextField)`
   };
 `;
 
-
 const SearchButton = () => {
   const [searchInput, setSearchInput] = useState("");
+  const debounceValue = useDebounce(searchInput, 600);
+
+  useEffect(() => {
+    console.log(debounceValue)
+  }, [debounceValue])
 
   const onChange = e => setSearchInput(e.target.value);
 
   return (
     <ButtonContainer>
-      {/* <InputBar 
-        autoFocus
-        onChange={onChange}
-        variant="outlined"
-        color="secondary"
-        label="search"
-        InputLabelProps={{
-          style:{
-            fontSize:"2rem",
-            color:"white"
-          }
-        }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="end">
-              <SearchButtonIcon />
-            </InputAdornment>
-          ),
-        }}
-        /> */}
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-            <SearchButtonIcon />
-          </Grid>
-          <Grid item>
-            <InputBar 
-              label="Search..."
-              autoFocus
-              color="secondary"
-              InputLabelProps={{
-                style:{
-                  fontSize:"1.5rem",
-                  color:"#aaa"
-                }
-              }}
-              onChange={onChange}
-              />
-          </Grid>
+      <Grid container spacing={1} alignItems="flex-end">
+        <Grid item>
+          <SearchButtonIcon />
         </Grid>
+        <Grid item>
+          <InputBar 
+            label="Search..."
+            autoFocus
+            color="secondary"
+            InputLabelProps={{
+              style:{
+                fontSize:"1.5rem",
+                color:"#aaa"
+              }
+            }}
+            onChange={onChange}
+            />
+        </Grid>
+      </Grid>
     </ButtonContainer>
   );
 }
